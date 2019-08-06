40ef72bddb6f3587322200b32e17bbf73fe20b89
Egor <egorka1391@gmail.com>
2019-08-06 21:22:29 +0500

    deploy by node

---
+++ index.js
@@ -0,0 +1,23 @@
+// содежимое index.js
+const http = require('http');
+const fs = require('fs');
+const port = 8000;
+const url = require('url');
+
+const requestHandler = (request, response) => {
+  const urlParts = url.parse(request.url, true);
+  const filePath = urlParts.pathname === '/' ?
+    './index.html' :
+    `./${urlParts.pathname}`;
+
+  fs.readFile(filePath, function(error, contents) {
+    response.end(contents);
+  });
+};
+
+const server = http.createServer(requestHandler);
+server.listen(port, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+});
